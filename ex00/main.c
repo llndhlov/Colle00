0e7252f630d9491504bb39784627ed4b2b1f2996
llndhlov <llndhlov@student.wethinkcode.co.za>
2020-07-15 17:50:28 +0200

    commit

---
+++ ex00/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llndhlov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 17:44:55 by llndhlov          #+#    #+#             */
+/*   Updated: 2020/07/15 17:45:14 by llndhlov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include	<unistd.h>
+
+void	ft_putchar(char	c);
+void	rush00(int	x, int	y);
+void	rush01(int	x, int	y);
+void	rush02(int	x, int	y);
+void	rush03(int	x, int	y);
+void	rush04(int	x, int	y);
+
+int		main(void)
+{
+				rush04(5, 5);
+				return	(0);
+}
---
+++ ex00/rush03.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llndhlov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 17:41:57 by llndhlov          #+#    #+#             */
+/*   Updated: 2020/07/15 17:42:04 by llndhlov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+void ft_putchar(char    c);
+void rush03(int     x, int      y)
+{
+                int 	counter;
+                int 	num_columns1;
+				int		num_columns2;
+                
+                counter = 0;
+                num_columns1 = x;
+				num_columns2 = x;
+                if(y > 0 && x > 0)
+                {
+                    ft_putchar('A');
+                    if(x > 1)
+                    {
+                    while(x > 1)
+                    {
+                                    ft_putchar('B');
+                                    x--;
+                    }
+                    ft_putchar('A');
+                    }
+                    while(y > 1)
+                    {           ft_putchar('\n');
+                                ft_putchar('B');
+                                while(num_columns1 > 1)
+                                {
+                                                ft_putchar(' ');
+												num_columns1--;
+                                }
+                                ft_putchar('B');
+                                y--;
+								num_columns1 = num_columns2;
+                    }
+                    ft_putchar('\n');
+                    ft_putchar('C');
+                    while(num_columns2 > 1)
+                    {
+                                    ft_putchar('B');
+                                    num_columns2--;
+                    }
+                    ft_putchar('C');
+                }
+                ft_putchar('\n');
+}
